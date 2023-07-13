e74524dcb0a71952be885177f66960657800134d
heran <herangenale7@gmail.com>
2023-07-13 09:53:49 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - start main program
+ * Return: always 0 if success
+ * non zero not success
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
