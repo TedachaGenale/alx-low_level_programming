6ea91a72e72a675c28d75df930218df4372f6ab6
heran <herangenale7@gmail.com>
2023-07-13 10:29:22 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - start main function
+ * Return: always 0 if success
+ * non zero not success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
