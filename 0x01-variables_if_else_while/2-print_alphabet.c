8ed965d0e476ab01e507da7f0864eec187ba71ba
heran <herangenale7@gmail.com>
2023-07-14 09:02:54 -0700

    program to print lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: always 0 if success
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
