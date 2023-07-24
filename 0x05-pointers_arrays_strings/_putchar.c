6f349cfb1945fbbdd616dca452fd283225a9b8b8
heran <herangenale7@gmail.com>
2023-07-24 13:40:13 -0700

    a function that prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - Function to print characters on std out
+ * @c: Character to be printed
+ * Return: Always 1 (success)
+ * -1 not success
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
